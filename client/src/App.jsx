dc8424b724fbab81cda604166743da56a72f0a64
daniyal-shams <hussaindaniyal748@gmail.com>
2025-06-26 05:07:40 +0530

    Home page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,12 +9,13 @@ import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 
 
 const App = () => {
   return (
-    <div className='mx-4 sm:mx=[10%]'>
+    <div className='mx-4 sm:mx-[10%]'>
       <Navbar />
       <Routes>
         <Route path='/' element={ <Home /> } />
@@ -28,6 +29,8 @@ const App = () => {
         <Route path='/Appointment/:docId' element={<Appointment /> }/>
 
       </Routes>
+
+      <Footer />
     </div>
   )
 }
