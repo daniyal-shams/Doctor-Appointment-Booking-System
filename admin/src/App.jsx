4fcfd82576e01f1edac05106aaa9b89c593b100e
daniyal-shams <hussaindaniyal748@gmail.com>
2025-06-28 03:30:32 +0530

    NavBar and SideBar Completed ✔️✔️

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
-import Login from './pages/Login'
-import { ToastContainer, toast } from 'react-toastify';
-import { useContext } from 'react';
-import {AdminContext} from './context/AdminContext'
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
+import React from "react";
+import Login from "./pages/Login";
+import { ToastContainer, toast } from "react-toastify";
+import { useContext } from "react";
+import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import {  Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/admin/Dashboard";
+import AllAppointments from "./pages/admin/AllAppointments";
+import AddDoctor from "./pages/admin/AddDoctor";
+import DoctorsList from "./pages/admin/DoctorsList";
+
+
 
 const App = () => {
-    const {aToken} = useContext(AdminContext ) ;
+  const { aToken } = useContext(AdminContext);
   return aToken ? (
-    <div className='bg-[#F8F9FD]'>
-        <ToastContainer />
-        <Navbar />
-        <div className='flex items-start'>
-            <Sidebar />
-        </div>
-    </div>
-    )
-    : ( 
-    <>
-        <Login />
-        <ToastContainer />
-    </> 
-    )
-}
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+      <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes >
+            <Route path="/" element={<Dashboard/>} />
+            <Route path='/admin-dashboard' element={<Dashboard /> } />
+            <Route path='/all-appointments' element={<AllAppointments />  } />
+            <Route path='/add-doctor' element={<AddDoctor /> } />
+            <Route path='/doctor-list' element={<DoctorsList /> } />
 
-export default App
+
+        </Routes>
+      </div>
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
+  );
+};
+
+export default App;
---
+++ admin/src/pages/admin/DoctorsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DoctorList = () => {
+  return (
+    <div>
+      Doctors List
+    </div>
+  )
+}
+
+export default DoctorList
