e6880b854f19e81bb7df180dfc08f577c98fad8d
daniyal-shams <hussaindaniyal748@gmail.com>
2025-06-27 20:14:46 +0530

    Admin Login Page Completed ✅✅

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter} from 'react-router-dom' 
+import AdminContextProvider from './context/AdminContext.jsx'
+import DoctorContextProvider from './context/DoctorContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+createRoot(document.getElementById('root')).render(  
+    
+    <BrowserRouter>
+     <AdminContextProvider >
+      <DoctorContextProvider >
+        <App />
+      </DoctorContextProvider>
+     </AdminContextProvider>
+    </BrowserRouter>
+
 )
