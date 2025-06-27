00c929da098d51393454f73d077ddbc828b72836
daniyal-shams <hussaindaniyal748@gmail.com>
2025-06-27 17:15:33 +0530

    Doctor API's created with auth model

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     mongoose.connection.on('connected', () => console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/HealthCareAppointmentDB`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/HealthCareAppointmentDB?retryWrites=true&w=majority&appName=Cluster0`);
 }
 
 export default connectDB;
