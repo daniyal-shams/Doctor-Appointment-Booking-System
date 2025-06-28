6bf5a11b5fa98ae126765977e08f509e84ffbc23
daniyal-shams <hussaindaniyal748@gmail.com>
2025-06-28 05:57:14 +0530

    Get all Doctors api completed ✔️✔️

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { createContext } from "react";
+import {toast} from 'react-toastify';
+import axios from 'axios';
 
 export const AdminContext = createContext();
 
@@ -8,11 +10,29 @@ const AdminContextProvider = (props) => {
     const [aToken,setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken') : '');
     
     const backendUrl = import.meta.env.VITE_BACKEND_URL ;
+    const [doctors, setDoctors] = useState([]);
+
+    const getAllDoctors = async () => {
+        try {
+            const {data} = await axios.post(backendUrl + '/api/admin/all-doctors', {},{headers:{aToken}});
+            if(data.success) {
+                setDoctors(data.doctors)
+                console.log(data.doctors)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    };
 
     const value = {
         aToken,setAToken,
-        backendUrl
+        backendUrl,
+        doctors,
+        getAllDoctors
     }
+
 
     return (
         <AdminContext.Provider value={value}>
