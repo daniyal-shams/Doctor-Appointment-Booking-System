12b7ab12b2951ce2d161c1153493992b8f075560
daniyal <109526438+daniyal-shams@users.noreply.github.com>
2025-07-04 00:27:54 +0530

    Update DoctorContext.jsx

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -7,7 +7,7 @@ export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
     
-    const backendUrl = import.meta.env.VITE_BACKEND_URL ;
+    const backendUrl = "https://doctor-appointment-booking-system-ykjs.onrender.com" ;
 
     const [dToken,setDToken] = useState(localStorage.getItem('dToken')?localStorage.getItem('dToken') : '');
 
