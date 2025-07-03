8248f72ad0ed52d83c525920219e8488cc879c53
daniyal <109526438+daniyal-shams@users.noreply.github.com>
2025-07-04 00:28:24 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 const AppContextProvider = (props) => {
 
     const currencySymbol = '$';
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const backendUrl = "https://doctor-appointment-booking-system-ykjs.onrender.com";
     const [doctors,setDoctors] = useState([]);
     const [token,setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
     const [userData, setUserData] = useState(false);
